18e78bbba2c54ae4b01576ef5d653907243e92e1
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-05-10 23:13:55 +0530

    fix bugs

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
+    phone: { type: String, required: true, sparse: true },
     branch: { type: String, required: true },
   },
   { timestamps: true }
